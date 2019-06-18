03d1f60ac1a9e65fad022b479f23d55f34de768a
Joachim J <Ich_bin_Joachim@yahoo.com>
2019-06-18 10:30:48 +0200

    precach

--- game/dota_addons/addon_template_butt/scripts/vscripts/addon_game_mode.lua
+++ game/dota_addons/addon_template_butt/scripts/vscripts/addon_game_mode.lua
@@ -23,10 +23,10 @@ require("startitems")
 require("thinker")
 
 function Precache( context )
+	PrecacheResource("soundfile", "soundevents/custom_sounds.vsndevts", context)
 	--[[
 		Precache things we know we'll use.  Possible file types include (but not limited to):
 			PrecacheResource( "model", "*.vmdl", context )
-			PrecacheResource( "soundfile", "*.vsndevts", context )
 			PrecacheResource( "particle", "*.vpcf", context )
 			PrecacheResource( "particle_folder", "particles/folder", context )
 	]]
