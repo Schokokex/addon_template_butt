ecea72f777cbac2f5d811a3693f869c656ffe380
Joachim J <Ich_bin_Joachim@yahoo.com>
2019-06-26 23:52:13 +0200

    gameevents

---
+++ game/dota_addons/addon_template_butt/scripts/vscripts/utils/custom_gameevents.lua
@@ -0,0 +1,23 @@
+local spawnedNPCs = {}
+ListenToGameEvent("npc_spawned", function(keys)
+	-- entindex numbers repeat after a while 
+	local npc = EntIndexToHScript(keys.entindex)
+	if (spawnedNPCs[keys.entindex] ~= npc) then
+		spawnedNPCs[keys.entindex] = npc
+		FireGameEvent("npc_first_spawn", {entindex = keys.entindex, unit = npc})
+	end
+end, nil)
+
+local l1 = 0
+local l2 = 0
+function fireGME()
+	local gME = GameRules:GetGameModeEntity()
+	if (gME) then 									-- comes with the first Buildings
+		FireGameEvent("created_game_mode_entity", {gameModeEntity = gME})
+		StopListeningToGameEvent(l1)
+		StopListeningToGameEvent(l2)
+	end
+end
+l1 = ListenToGameEvent("npc_spawned", fireGME, nil)
+l2 = ListenToGameEvent("game_rules_state_change", fireGME, nil) -- backup
+
