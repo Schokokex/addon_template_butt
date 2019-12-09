3f03f155567c68246ea85391850815facc7898ac
Jochnickel <Jochnickel@github.com>
2019-12-09 02:37:41 +0100

    quickfix

--- game/dota_addons/addon_template_butt/scripts/vscripts/addon_game_mode.lua
+++ game/dota_addons/addon_template_butt/scripts/vscripts/addon_game_mode.lua
@@ -1,6 +1,8 @@
 _G.ADDON_FOLDER = debug.getinfo(1,"S").source:sub(2,-37)
-_G.PUBLISH_DATA = LoadKeyValues(ADDON_FOLDER:sub(5,-16).."publish_data.txt")
-_G.WORKSHOP_TITLE = PUBLISH_DATA.title -- LoadKeyValues(debug.getinfo(1,"S").source:sub(7,-53).."publish_data.txt").title 
+if not IsInToolsMode() then
+	_G.PUBLISH_DATA = LoadKeyValues(ADDON_FOLDER:sub(5,-16).."publish_data.txt")
+	_G.WORKSHOP_TITLE = PUBLISH_DATA.title -- LoadKeyValues(debug.getinfo(1,"S").source:sub(7,-53).."publish_data.txt").title 
+end
 
 _G.GameMode = _G.GameMode or class({})
 
