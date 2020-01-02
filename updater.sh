32c66128842caced30944b049ce6407b8505c227
Jochnickel <47549862+Jochnickel@users.noreply.github.com>
2020-01-02 12:27:56 +0100

    Update updater.sh

--- updater.sh
+++ updater.sh
@@ -27,6 +27,7 @@ git add units.txt -f
 git add herolist.txt -f
 git commit -m "Auto-Update from https://github.com/SteamDatabase/GameTracking-Dota2"
 git push
+cd ~/butt/addon_template_butt/
 git branch master_nomap
 git checkout master_nomap
 git rm *dota.vmap
