c8bfdc566938d22d7d3e6c5ebb075e2be975f3fa
Jochnickel <joachim.redmi@gmail.com>
2020-01-25 00:03:59 +0100

    autocopy was not helpful

--- game/dota_addons/addon_template_butt/scripts/vscripts/internal/init.lua
+++ game/dota_addons/addon_template_butt/scripts/vscripts/internal/init.lua
@@ -7,7 +7,7 @@ if IsInToolsMode() then
 		GameRules:GetGameModeEntity():SetThink(function()
 			if IsSchokokeks() then -- just works in addon_template_butt folder
 				local dotaFolder = ADDON_FOLDER:sub(0,-38)
-				pcall(copyFile, dotaFolder.."content/dota/maps/dota.vmap", dotaFolder .. "content/dota_addons/addon_template_butt/maps/dota.vmap")
+				-- pcall(copyFile, dotaFolder.."content/dota/maps/dota.vmap", dotaFolder .. "content/dota_addons/addon_template_butt/maps/dota.vmap")
 				return
 			end
 			local agmStr = fileToString(ADDON_FOLDER .. "scripts/vscripts/addon_game_mode.lua")
@@ -22,9 +22,9 @@ if IsInToolsMode() then
 	if not addInf.maps then	addInf.maps = "dota" end
 	kvToFile({AddonInfo = addInf}, ADDON_FOLDER .. "addoninfo.txt")
 
-	copyFile("../../dota/scripts/npc/npc_units.txt", ADDON_FOLDER .. "scripts/npc/units.txt")
-	copyFile("../../dota/scripts/npc/npc_abilities.txt", ADDON_FOLDER .. "scripts/npc/abilities.txt")
-	copyFile("../../dota/scripts/npc/npc_heroes.txt", ADDON_FOLDER .. "scripts/npc/heroes.txt")
+	-- copyFile("../../dota/scripts/npc/npc_units.txt", ADDON_FOLDER .. "scripts/npc/units.txt")
+	-- copyFile("../../dota/scripts/npc/npc_abilities.txt", ADDON_FOLDER .. "scripts/npc/abilities.txt")
+	-- copyFile("../../dota/scripts/npc/npc_heroes.txt", ADDON_FOLDER .. "scripts/npc/heroes.txt")
 	-- print(fileToString("http://github.com/SteamDatabase/GameTracking-Dota2/blob/master/game/dota/pak01_dir/scripts/npc/items.txt"))
 
 end
