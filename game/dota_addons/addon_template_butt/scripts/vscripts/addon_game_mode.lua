6aa9314a73625ca5b487bd2675ebc5f70a5311ff
Jochnickel <47549862+Jochnickel@users.noreply.github.com>
2020-06-05 14:35:36 +0200

    Update addon_game_mode.lua

---
+++ game/dota_addons/addon_template_butt/scripts/vscripts/addon_game_mode.lua
@@ -0,0 +1,27 @@
+-- This is the root file
+--
+-- Changes are meant to be done in GameMode.lua
+--
+--
+--
+--
+-- This file is supposed to load the Butt Framework and the individual GameMode
+-- Also it will trigger events on Precache, Spawn and Activate
+
+
+-- TODO:
+-- require "Butt.Core"
+-- softRequire "Butt.Helper"
+
+
+function Precache( context )
+	-- TODO: trigger Event
+end
+
+function Spawn()
+	-- TODO: trigger Event
+end
+
+function Activate()
+	-- TODO: trigger Event
+end
